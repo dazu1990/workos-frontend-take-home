408d1cdd8acf46aac0c879585cbc2150bd7c1a60
dazu1990 <adrian.davilazuniga@gmail.com>
2025-06-19 10:54:55 -0400

    clean up template + installed radix theme + added theme config

---
+++ client/src/config/theme.ts
@@ -0,0 +1,10 @@
+import type { ThemeProps } from "@radix-ui/themes";
+
+export const themeConfig: ThemeProps = {
+  appearance: "light",
+  accentColor: "purple",
+  grayColor: "gray",
+  radius: "medium",
+  scaling: "100%",
+  panelBackground: "solid",
+};
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
+import "@radix-ui/themes/styles.css";
 import { createRoot } from "react-dom/client";
+import { themeConfig } from "./config/theme";
+
 import "./index.css";
 import App from "./App.tsx";
+import { Theme } from "@radix-ui/themes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Theme {...themeConfig}>
+      <App />
+    </Theme>
+  </StrictMode>
 );
