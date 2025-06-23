fb52676d5c878eb775f978e38297263667bf4e3a
dazu1990 <adrian.davilazuniga@gmail.com>
2025-06-23 09:08:16 -0400

    accessibilty for tabs

--- client/src/views/DataTable/Datatable.tsx
+++ client/src/views/DataTable/Datatable.tsx
@@ -4,16 +4,42 @@ import { RolesTable } from "./components/RolesTable";
 
 export const DataTables = () => {
   return (
-    <Tabs.Root defaultValue="users" orientation="horizontal">
-      <Tabs.List>
-        <Tabs.Trigger value="users">Users</Tabs.Trigger>
-        <Tabs.Trigger value="roles">Roles</Tabs.Trigger>
+    <Tabs.Root
+      defaultValue="users"
+      orientation="horizontal"
+      aria-label="User and Role Data Tables"
+    >
+      <Tabs.List aria-label="Data Table Tabs">
+        <Tabs.Trigger
+          value="users"
+          aria-controls="users-tabpanel"
+          aria-label="Show Users Table"
+        >
+          Users
+        </Tabs.Trigger>
+        <Tabs.Trigger
+          value="roles"
+          aria-controls="roles-tabpanel"
+          aria-label="Show Roles Table"
+        >
+          Roles
+        </Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content value="users">
-        <UsersTable></UsersTable>
+      <Tabs.Content
+        value="users"
+        id="users-tabpanel"
+        aria-label="Users Table Panel"
+        tabIndex={0}
+      >
+        <UsersTable />
       </Tabs.Content>
-      <Tabs.Content value="roles">
-        <RolesTable></RolesTable>
+      <Tabs.Content
+        value="roles"
+        id="roles-tabpanel"
+        aria-label="Roles Table Panel"
+        tabIndex={0}
+      >
+        <RolesTable />
       </Tabs.Content>
     </Tabs.Root>
   );
